7c5beb8867db79ac2993c01a03c86707b4d5527f
DEVA NANDAN K V <69035490+obsidian-66@users.noreply.github.com>
2026-08-11 07:17:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function createDetector() {
     baseOptions: {
       modelAssetPath:
         "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite",
-      delegate: "GPU"
+      delegate: "CPU"
     },
     runningMode: "VIDEO",
     minDetectionConfidence: 0.5
